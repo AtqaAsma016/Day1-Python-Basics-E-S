c9c91fa4c8e6e9d4049b3e8a2a351ef1d123c77c
AtqaAsma016 <01-131232-016@student.bahria.edu.pk>
2025-07-01 09:12:04 +0100

    initial commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+"""
+#*************DAY 1*************
 #Trafic Light
 light = input("Light : ")
 if (light == "red"):
@@ -34,4 +36,66 @@ if a>=b:
     print("True")
 else:
     print("False")
-# 3456
+
+#*************DAY 2*************
+str= "atqa asma"
+print (str[2: 6])
+print(str.endswith("ma"))
+str=str.capitalize()
+print(str)
+print(str.replace("as", "As"))
+print(str.count("a"))
+print(str.find("a"))
+
+
+#WAP to input users first name and print its lenght
+first_name= input("Enter name: ")
+lenght= len(first_name)
+print(f"Lenght of string is: {lenght}")
+
+
+#WAP to find occurance of "$" in string
+str1= "1$234$67@&"
+str1count=str1.count("$")
+print(f"No. of times $ occured in \"{str1}\" is {str1count}")
+
+#Grade students base don marks
+marks= int(input("Enter marks: "))
+if marks>=90 and marks<=100:
+    print("Grade A")
+elif marks>=80 and marks<90:
+    print("Grade: B")
+elif marks>=70 and marks<80:
+    print("Grade: C")  
+elif marks<70 and marks>=0:
+    print("Grade: B")  
+else :
+    print("Invalid")     
+
+#WAP to check if the number entered by user is even or odd
+number= int(input("Enter number: "))
+if ((number % 2) == 0):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")    
+
+#WAP to find greatest of three numbers
+num1= int(input("Enter number 1: "))
+num2= int(input("Enter number 2: "))
+num3= int(input("Enter number 3: "))
+if num1>num2 and num1>num3:
+    greatest= num1
+elif num2>num1 and num2>num3:
+    greatest= num2
+elif num3>num1 and num3>num2:
+    greatest= num3
+print(f"Greatest among {num1} {num2} {num3} is {greatest}")    
+"""
+
+#WAP to check if the number is multiple of 7.
+num= int(input("Enter number: "))
+rem=num%7
+if rem==0
+    
+
+
